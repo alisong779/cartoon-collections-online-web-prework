e3c7ad7999754b800cf285d24d4bcb8bb5fcd472
Alison Golden <alisong779@yahoo.com>
2019-04-10 16:03:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,8 @@ end
 
 def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    arr.include?(cheese_types)
+    arr.select do |x|
+    x.include?(cheese_types)
 end 
 
     
