12a385ea718908b29c32f8d02d472c71e9a835f7
Alison Golden <alisong779@yahoo.com>
2019-04-10 16:49:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def find_the_cheese(arr)
     new_var = arr.select do |x|
     if cheese_types.include?(x) then return x
   end 
- if new_var == [] return nil 
+ if new_var == [] then return nil 
 end 
 end
     
